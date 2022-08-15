a35ec42b3ff827401fe8fa34d17e9fe19623a93e
blinky1994 <56903269+blinky1994@users.noreply.github.com>
2022-08-15 14:45:11 +0800

    update port for heroku

--- server.js
+++ server.js
@@ -34,4 +34,4 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db)});
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
 
-app.listen(3001, () => { console.log('Server is running on port: 3001');});
+app.listen(process.env.PORT || 3001, () => { console.log(`Server is running on port: ${process.env.PORT}`);});
