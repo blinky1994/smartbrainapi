92f7453c9632e6c67155382935ef0195b3fd44be
blinky1994 <56903269+blinky1994@users.noreply.github.com>
2022-09-01 20:13:40 +0800

    added docker

--- server.js
+++ server.js
@@ -7,19 +7,34 @@ const register = require('./controllers/register')
 const signin = require('./controllers/signin')
 const profile = require('./controllers/profile')
 const image = require('./controllers/image')
+const morgan = require('morgan');
 
+//For Heroku
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    port : 5432,
-    user : 'postgres',
-    password : 'test',
-    database : 'smart-brain'
+    connectionString: process.env.DATABASE_URL,
+     ssl: {
+    rejectUnauthorized: false
+    }
   }
 });
 
+//For Local
+// const db = knex({
+//   client: 'pg',
+//   connection: {
+//     host : '127.0.0.1',
+//     port : 5432,
+//     user : 'postgres',
+//     password : 'test',
+//     database : 'smart-brain'
+//   }
+// });
+
 app.use(express.json());
+app.use(morgan('combined'));
 app.use(cors());
 
 app.get('/', (req, res) => { res.send('success');});
@@ -34,4 +49,5 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db)});
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
 
-app.listen(3001, () => { console.log('Server is running on port: 3001');});
+// app.listen(3001, () => { console.log('Server is running on port: 3001');});
+app.listen(process.env.PORT || 3001, () => { console.log(`Server is running on port: ${process.env.PORT}`);});
