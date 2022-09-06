0cea24da3ab5484cee82a6d8fdaf0463793d72ae
blinky1994 <56903269+blinky1994@users.noreply.github.com>
2022-09-06 20:17:14 +0800

    added profile get and update endpoints

--- server.js
+++ server.js
@@ -8,33 +8,28 @@ const signin = require('./controllers/signin')
 const profile = require('./controllers/profile')
 const image = require('./controllers/image')
 const morgan = require('morgan');
+require('dotenv').config();
 
 //For Heroku
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
-const db = knex({
-  client: 'pg',
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-     ssl: {
-    rejectUnauthorized: false
-    }
-  }
-});
-
-//For Local
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
 // const db = knex({
 //   client: 'pg',
 //   connection: {
-//     host : '127.0.0.1',
-//     port : 5432,
-//     user : 'postgres',
-//     password : 'test',
-//     database : 'smart-brain'
+//     connectionString: process.env.DATABASE_URL,
+//      ssl: {
+//     rejectUnauthorized: false
+//     }
 //   }
 // });
 
+//For Local
+const db = knex({
+  client: 'pg',
+  connection: process.env.POSTGRES_URI
+});
+
 app.use(express.json());
-// app.use(morgan('combined'));
+app.use(morgan('combined'));
 
 app.use(cors());
 
@@ -46,9 +41,11 @@ app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcryp
 
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)});
 
+app.post('/profile/:id', (req, res) => { profile.handleProfileUpdate(req, res, db)});
+
 app.put('/image', (req, res) => { image.handleImage(req, res, db)});
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
 
-// app.listen(3001, () => { console.log('Server is running on port: 3001');});
-app.listen(process.env.PORT || 3001, () => { console.log(`Server is running on port: ${process.env.PORT}`);});
+app.listen(3001, () => { console.log('Server is running on port: 3001');});
+// app.listen(process.env.PORT || 3001, () => { console.log(`Server is running on port: ${process.env.PORT}`);});
